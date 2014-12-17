a055454992e29f7d80c7ffc5b34faaacb95d28c1
syany <kz.github.syany@gmail.com>
2014-12-18 00:27:02 +0900

    タイトルに日本語訳 著者の記載

--- 1.1.2/docs-jp/templates/footer.js
+++ 1.1.2/docs-jp/templates/footer.js
@@ -5,6 +5,8 @@ document.write('<tr>')
 
 document.write('<td valign="top">Copyright &copy; 2013  <a href="http://www.qos.ch/">QOS.ch</a></td>')
 
+document.write('<td valign="top">[翻訳著者] Copyright &copy; 2014  <a href="https://github.com/Logback-JP-Group/Logback-Manuals-JP">Logback-JP-Group</a>, <a href="http://ksyany.blog32.fc2.com/">syany</a></td>')
+
 document.write('  <td rowspan="2">');
 document.write('    <a href="http://twitter.com/qos_ch">');
 document.write('      <img alt="Follow @qos_ch" src="http://www.slf4j.org/images/follow_us.png" />');
@@ -18,9 +20,12 @@ AAT = '@'
 DOOTT = '.'
 document.write('<tr>') 
 
-document.write('<td align="left" colspan="1">') 
-document.write('We are actively looking for volunteers to proofread the documentation. Please send your corrections or suggestions for improvement to "corrections' + AAT +'qos'+DOOTT+'ch". See also the <a href="http://articles.qos.ch/contributing.html">instructions for contributors</a>.');
+document.write('<td align="left" colspan="2">') 
+document.write('We are actively looking for volunteers to proofread the documentation. Please send your corrections or suggestions for improvement to "corrections' + AAT +'qos'+DOOTT+'ch". See also the <a href="http://articles.qos.ch/contributing.html">instructions for contributors</a>.' +
+               '<br>本文章は、<code>Logback-JP-Group</code>が勝手に翻訳したものであり、原文内容との不整合などの責任は負いかねます。');
 document.write('</td>') 
+document.write('  <td>&nbsp;</td>')
+document.write('  <td>&nbsp;</td>')
 document.write('  <td>&nbsp;</td>')
 document.write('</tr>') 
 
